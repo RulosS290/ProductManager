c21c2afa114eb129e105eaa19c550d8d310d921e
Daniel Torres <ruloss29@outlook.es>
2025-11-03 18:27:26 -0500

    delete folder /dist

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var server_1 = require("./server");
-console.log('Desde index.ts');
-(0, server_1.sum)();
-//# sourceMappingURL=index.js.map
